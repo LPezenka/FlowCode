494df9af1b6d24a9512ef5f911c03b466bd44952
PerformanceEngineer <64775369+PerformanceEngineer@users.noreply.github.com>
2025-12-02 17:53:57 +0100

    Refactor
    A lot of refactoring to make the actionNode evaluation code more robust end easier to read

--- FlowCodeInfrastructure/ActionNode.cs
+++ FlowCodeInfrastructure/ActionNode.cs
@@ -21,38 +21,63 @@ namespace FlowCodeInfrastructure
 
         private (ScriptState, string) HandleAssignment(ScriptState scriptState, string code, ref bool initVariable, ref string varName)
         {
+            // Jump out of method if there is no assignment in the code
             if (Regex.IsMatch(code, @"(?<!\=)\=(?!\=)") == false)
                 return (scriptState, code);
+
             initVariable = false;
             var parts = code.Split("=");
             // TODO: something a = b == c doesn't work because of the split
             // fix that by finding the first occurence and manually taking substrings
 
             varName = parts[0].Trim();
-            var filterName = varName;
+            var filterName = varName; // Temp variable as refs cannot be used in Lamdas
             var v = ScriptState.Variables.Where(x => x.Name == filterName).FirstOrDefault();
             if (v is null)
             {
-                var rightHandVariable = parts[1].Trim();
-                var rhv = ScriptState.Variables.Where(x => x.Name == rightHandVariable).FirstOrDefault();
-                if (rhv is not null)
-                    code = $"string lineInput = {parts[1]}.ToString();";
-                else
-                        if (parts[1].Contains("*")
-                                || parts[1].Contains("+")
-                                || parts[1].Contains("-")
-                                || parts[1].Contains("/"))
-                {
-                    string rightHandCode = "var tv = " + parts[1] + ";";
-                    code = rightHandCode +
-                        System.Environment.NewLine +
-                        $"string lineInput = tv.ToString()";
-                }
-                else
-                    code = $"string lineInput = \"{parts[1]}\";";
-                initVariable = true;
+                // There is no variable whose name corresponds to the left side of the assignment
+                // Infer type of right hand side, allocate variable and assign value
+                (ScriptState, code) = HandleAssignmentToNew(ScriptState, code, ref initVariable, parts[1]);
             }
             return (scriptState, code);
+        }
+
+        private static (ScriptState, string) HandleAssignmentToNew(ScriptState scriptState, string code, ref bool initVariable, string rightHandPart)
+        {
+            var rightHandVariable = rightHandPart.Trim();
+            var rhv = scriptState.Variables.Where(x => x.Name == rightHandVariable).FirstOrDefault();
+
+            if (rhv is not null)
+                code = $"string lineInput = {rightHandPart}.ToString();";
+            else
+            {
+                //string pattern = @"^\s*[A-Za-z_][A-Za-z0-9_]*\s*(?:\+\+|--|[+\-*/]\s*[A-Za-z_0-9][A-Za-z0-9_]*)+\s*$";
+
+                //if (Regex.IsMatch(code, pattern))
+                if (rightHandPart.Contains("*")
+                        || rightHandPart.Contains("+")
+                        || rightHandPart.Contains("-")
+                        || rightHandPart.Contains("/")
+                        || rightHandPart.Contains("++")
+                        || rightHandPart.Contains("--"))
+                {
+                    string rightHandCode = "var tv = " + rightHandPart + ";";
+                    ScriptState tempState = scriptState.ContinueWithAsync(rightHandCode, ScriptOptions).Result;
+                    var tv = tempState.Variables.Where(x => x.Name == "tv").FirstOrDefault();
+                    if (tv is not null)
+                    {
+                        code = $"string lineInput = \"{tv.Value.ToString().Trim()}\"";
+                        //scriptState
+                        //code = //rightHandCode +
+                        //System.Environment.NewLine +
+                        //    $"string lineInput = tv.ToString()";
+                    }
+                    else
+                        code = $"string lineInput = \"{rightHandPart.Trim()}\";";
+                }
+            }
+                initVariable = true;
+                return (ScriptState, code);
         }
 
         public override void Evaluate()
@@ -92,7 +117,7 @@ namespace FlowCodeInfrastructure
                     else
                     {
                         Code = Code.Replace(";", "");
-                        Code = Code.Replace("Ausgabe:", "Console.WriteLine(");
+                        Code = Code.Replace(Config.GetKeyword(Config.KeyWord.Output), "Console.WriteLine(");
                         Code += ");";
                     }
                 }
@@ -114,42 +139,13 @@ namespace FlowCodeInfrastructure
                     string varType = "string;";
                     initVariable = true;
                 }
-                else if (Code.Contains("="))// && !Code.Contains("=="))
-                {
-                    (ScriptState, Code) = HandleAssignment(ScriptState, Code, ref initVariable, ref varName);
-                    //var parts = Code.Split("=");
-                    //varName = parts[0].Trim();
 
-                    //// TODO: recognize arithmetics on the right side of the assignment operator, evaluate,
-                    //// store the result into a temporary variable and then assign it
-
-                    //var v = ScriptState.Variables.Where(x => x.Name == varName).FirstOrDefault();
-                    //if (v is null)
-                    //{
-                    //    var rightHandVariable = parts[1].Trim();
-                    //    var rhv = ScriptState.Variables.Where(x => x.Name == rightHandVariable).FirstOrDefault();
-                    //    if (rhv is not null)
-                    //        Code = $"string lineInput = {parts[1]}.ToString();";
-                    //    else
-                    //            if (       parts[1].Contains("*")
-                    //                    || parts[1].Contains("+")
-                    //                    || parts[1].Contains("-")
-                    //                    || parts[1].Contains("/"))
-                    //    {
-                    //        string rightHandCode = "var tv = " + parts[1] + ";";
-                    //        Code = rightHandCode +
-                    //            System.Environment.NewLine +
-                    //            $"string lineInput = tv.ToString()";
-                    //    }
-                    //    else
-                    //        Code = $"string lineInput = \"{parts[1]}\";";
-                    //    initVariable = true;
-                    //}
-                }
+                (ScriptState, Code) = HandleAssignment(ScriptState, Code, ref initVariable, ref varName);
 
                 if (Code.Contains(Config.GetKeyword(Config.KeyWord.Function)))
                 {
-                    Console.WriteLine($"Entering {Code}");
+                    // TODO: Improve Logging. Simply writing to the console won't do
+                    //Console.WriteLine($"Entering {Code}");
                 }
                 else
                 {
@@ -167,7 +163,10 @@ namespace FlowCodeInfrastructure
                         OutputHandler.ShowOutput(outputText);
                     }
                     else
+                    {
+                        // TODO: Consider moving this or find another way to get rid of lineInput
                         ScriptState = ScriptState.ContinueWithAsync(Code, ScriptOptions).Result;
+                    }
                     if (initVariable)
                     {
                         var v = ScriptState.Variables.Where(x => x.Name == "lineInput").FirstOrDefault();
@@ -206,17 +205,12 @@ namespace FlowCodeInfrastructure
             }
             catch(CompilationErrorException cee)
             {
-                //Console.WriteLine(cee.ToString());
-                //if (Code.Contains("="))
-                //{
-                //    var parts = Code.Split("=");
-                //    var varname = parts[0];
-                //}
+                // TODO: log to file
                 throw cee;
             }
             catch (Exception ex)
             {
-                //Console.WriteLine(ex.ToString());
+                // TODO: log to file
                 throw ex;
             }
         }
